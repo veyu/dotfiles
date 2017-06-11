b5fcf340f3765478593e8289bc55424b8aebc188
veyu <duplikatmail@gmail.com>
2017-06-11 23:06:48 +0200

    plugins: add fugitive

--- setup_vim.sh
+++ setup_vim.sh
@@ -6,6 +6,9 @@ git clone https://github.com/tpope/vim-pathogen.git
 cp vim-pathogen/autoload/pathogen.vim ~/.vim/autoload
 rm -rf vim-pathogen
 
+# fugitive
+git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+
 # lightline
 git clone https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline.vim
 
